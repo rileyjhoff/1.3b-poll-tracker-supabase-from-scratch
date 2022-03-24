9e03b7485361fd8e15a3be87a776216cc4e8cd4f
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-24 10:12:55 -0700

    template setup - linked to supabase & added necessary files

---
+++ fetch-utils.js
@@ -0,0 +1,4 @@
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpobW93Z2N5YnRlcWdpd3dyeGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5OTQ2NjYsImV4cCI6MTk2MzU3MDY2Nn0.e8IeeowEcZ9C7aazuyONAepUhFvdOgDSLq8EKRJWwls';
+const SUPABASE_URL = 'https://zhmowgcybteqgiwwrxln.supabase.co';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
