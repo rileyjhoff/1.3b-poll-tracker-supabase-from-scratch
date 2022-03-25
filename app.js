abd066db56915b86ca454bbbc3a994d66dee18e6
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-25 09:45:20 -0700

    wrote sign in/up form submit listeners and supabase functions to sign in/up user, redirect to polls page, and check sessions on the poll page

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 // import functions and grab DOM elements
+import { signInUser, signUpUser, redirectToPolls } from './fetch-utils.js';
+
+const signInForm = document.getElementById('sign-in');
+const signUpForm = document.getElementById('sign-up');
 
 // let state
+redirectToPolls();
 
 // set event listeners 
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    const user = await signInUser(data.get('email'), data.get('password'));
+
+    if (user) {
+        redirectToPolls();
+    } else {
+        console.error(user);
+    }
+});
+
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    const user = await signUpUser(data.get('email'), data.get('password'));
+
+    if (user) {
+        redirectToPolls();
+    } else {
+        console.error(user);
+    }
+});
--- fetch-utils.js
+++ fetch-utils.js
@@ -2,3 +2,33 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZ
 const SUPABASE_URL = 'https://zhmowgcybteqgiwwrxln.supabase.co';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
+
+export async function signUpUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+
+    return response.user;
+}
+
+export async function getUser() {
+    return client.auth.session();
+}
+
+export async function checkSession() {
+    const user = await getUser();
+
+    if (!user) {
+        location.replace('../');
+    }
+}
+
+export async function redirectToPolls() {
+    if (await getUser()) {
+        location.replace('./polls');
+    }
+}
